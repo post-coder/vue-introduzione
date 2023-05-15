63b2508d87987fb1d52daa21736b1d4d20b27ba1
post-coder <gabrielspanu96@gmail.com>
2023-05-15 11:36:24 +0200

    feat: prepare to branches exercise

--- js/script.js
+++ js/script.js
@@ -8,46 +8,14 @@ const { createApp } = Vue
       return {
         // dentro questo oggetto possiamo inserire tutte le proprietà che vogliamo
         title: "Primo approccio a Vue",
-        nome: "Gabriel",
+        nome: "Edoardo",
         alunni: ["luca", "edoardo", "niccolò"],
-        umore: "bene",
-        titleClass: 'red',
-        inputUtente: "",
       }
     },
 
 
     methods: {
-      // qui possiamo scrivere le nostre funzioni
-      // da utilizzare nella nostra pagina
-      // ricordiamoci che questi sono metodi
-      // dell'oggetto methods{}
-      // in quanto tali vanno separati con la virgola
-      changeTitleClass() {
-        // prendere ciò che è scritto dentro l'input
-        // inserirlo come classe del titolo
-
-        // non modifichiamo più direttamente gli elementi in pagina
-        // modifichiamo invece le proprietà di data()
-        // che verranno aggiornate in pagina
-
-        // Vue ci permette di accedere alle proprietà
-        // dell'oggetto restituito in data() con
-        this.titleClass = this.inputUtente;
-        
-      },
-      // ^ ecco la virgola che li separa
-
-
-      // funzione che al click del titolo ne cambia il testo
-      changeText() {
-        
-        // siccome il titolo ha come testo la nostra proprietà "title" interpolata all'interno con le {{ }}
-        // possiamo cambiare direttamente lei e il cambiamento verrà rispecchiato in pagina
-        this.title = this.inputUtente;
-        
-      },
-
+      
     },
 
 }).mount('#app');
